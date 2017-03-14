84f71617e419beca6c3452fd184e801d39af4dfc
muye193 <yuanlei193@126.com>
2017-03-14 13:09:14 +0800

    php代码

---
+++ client.html.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>留言查询</title>
+</head>
+<body>
+<div>
+	<form method="get" action="index.php">
+    <input type="submit" value="创建表单" name="creat"></input>
+    <input type="submit" value="查询数据" name="Inquire"></input>
+    </form>
+</div>
+</body>
+</html>
---
+++ succes.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>成功</title>
+</head>
+<h1>
+<?php 
+	echo '创建表单成功';
+?>
+<h1>
+<body>
+</body>
+</html>
