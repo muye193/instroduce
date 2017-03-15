71eb0f4018ba81528327d2cf44e2665eb40f982d
muye193 <yuanlei193@126.com>
2017-03-15 11:11:50 +0800

    php

--- list.html.php
+++ list.html.php
@@ -5,28 +5,20 @@
 <title>查询列表</title>
 </head>
 <body>
-<?php
-function html($abb){
-	return htmlspecialchars($abb, ENT_QUOTES,'UTF-8');
-	}
-function htmlout($abb){
-	echo html($text);
-	}
-?>
 <div>
 	<ul>
     	<?php foreach ($result as $row):?>
-        	<li><?php htmlout($row['name'])?></li>
+        	<li><?php echo htmlspecialchars($row['name'],ENT_QUOTES,'UTF-8')?></li>
         <?php endforeach;?>
     </ul>
     <ul>
     	<?php foreach ($result as $row):?>
-        	<li><?php htmlout($row['email'])?></li>
+        	<li><?php echo htmlspecialchars($row['email'],ENT_QUOTES,'UTF-8')?></li>
         <?php endforeach;?>
     </ul>
     <ul>
     	<?php foreach ($result as $row):?>
-        	<li><?php htmlout($row['text'])?></li>
+        	<li><?php echo htmlspecialchars($row['text'],ENT_QUOTES,'UTF-8')?></li>
         <?php endforeach;?>
     </ul>
 </div>
